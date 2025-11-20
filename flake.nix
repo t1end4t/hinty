1023fba1fb8d247b7d5bf96c0e47436a097518c9
t1end4t <tiendatarse3004@gmail.com>
2025-11-20 14:45:22 +0700

    move hinty to src

--- flake.nix
+++ flake.nix
@@ -2,10 +2,8 @@
   inputs = {
     nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     systems.url = "github:nix-systems/default";
-
     devenv.url = "github:cachix/devenv";
     devenv.inputs.nixpkgs.follows = "nixpkgs";
-
     nixpkgs-python = {
       url = "github:cachix/nixpkgs-python";
       inputs = {
@@ -31,40 +29,50 @@
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
-      # ------------------------------------------------------------------
-      # 1. PACKAGES: Import the package definition from packages/hinty.nix
-      # ------------------------------------------------------------------
-      packages = forEachSystem (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          # Define python for simplicity
-          python = pkgs.python311;
-        in
-        {
-          # Use callPackage to load the function and pass arguments automatically
-          hinty = pkgs.callPackage ./packages/hinty.nix {
-            inherit python;
-            pythonPackages = python.pkgs;
-          };
-
-          # Make the package runnable with 'nix run .'
-          default = self.packages.${system}.hinty;
-        }
-      );
-
-      # ------------------------------------------------------------------
-      # 2. DEV SHELLS: Import the devShell definition from devshell.nix
-      # ------------------------------------------------------------------
       devShells = forEachSystem (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          # Use callPackage to load the devshell configuration
-          default = pkgs.callPackage ./devshell.nix {
-            inherit inputs pkgs devenv;
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = with pkgs; [
+                  pyright # python lsp
+                  ruff # fast linter
+                ];
+
+                # https://devenv.sh/reference/options/
+                languages.python = {
+                  enable = true;
+                  version = "3.11";
+                  uv = {
+                    enable = true;
+                    sync.enable = true;
+                  };
+                };
+
+                scripts.init-project.exec = ''
+                  ${pkgs.uv}/bin/uv init
+                  ${pkgs.uv}/bin/uv sync
+                  ${pkgs.uv}/bin/uv add --dev -r dev-requirements.txt
+                '';
+
+                enterShell = ''
+                  source .devenv/state/venv/bin/activate
+                '';
+
+                env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc
+                  pkgs.zlib
+                  pkgs.libGL
+                  pkgs.glib
+                ];
+              }
+            ];
           };
         }
       );
