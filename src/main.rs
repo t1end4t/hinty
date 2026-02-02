7a056ab510eb77252bc9df9d1258131cca45bd75
t1end4t <tiendatarse3004@gmail.com>
2026-02-02 11:28:15 +0700

    remove all, keep thing easy to implement

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use rust_template::sorts::bead_sort;
-
-fn main() {
-    let arr = vec![6, 11, 12, 4, 1, 5];
-    let result = bead_sort(arr);
-
-    // print out result
-    println!("Sorted array: {:?}", result);
-}
